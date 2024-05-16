a6514d10f63fa5408e89eb7b4c5097659ce96a35
Hannes Hering <hanneshering1999@gmail.com>
2024-05-16 19:42:12 +0200

    padding fix

--- portfolio-hering/pages/projects/index.vue
+++ portfolio-hering/pages/projects/index.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="bg-bg-primary text-text-primary">
+    <div class="bg-bg-primary text-text-primary p-6">
         <div class="font-rekron text-5xl">
             Projects
+            <svg class="h-4 mt-2" viewBox="0 0 100 1" xmlns="http://www.w3.org/2000/svg">
+                <line x1="0" y1="0" x2="40" y2="0" stroke="#F6690C" stroke-width="1" />
+            </svg>
         </div>
     </div>
 </template>
