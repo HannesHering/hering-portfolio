46e447044e02d93abd4099e557937eb4e4358ae7
Hannes Hering <hanneshering1999@gmail.com>
2024-05-18 20:16:11 +0200

    fixed bg color in slug

--- portfolio-hering/pages/projects/[slug].vue
+++ portfolio-hering/pages/projects/[slug].vue
@@ -2,23 +2,25 @@
 
 <template>
     <main>
-        <div class="bg-bg-primary text-text-primary p-6">
-            <div class="font-rekron text-5xl">
-                Projects
-                <svg class="h-4 mt-2" viewBox="0 0 100 1" xmlns="http://www.w3.org/2000/svg">
-                    <line x1="0" y1="0" x2="40" y2="0" stroke="#F6690C" stroke-width="1" />
-                </svg>
+        <div class="bg-bg-primary">
+            <div class="text-text-primary p-6">
+                <div class="font-rekron text-5xl">
+                    Projects
+                    <svg class="h-4 mt-2" viewBox="0 0 100 1" xmlns="http://www.w3.org/2000/svg">
+                        <line x1="0" y1="0" x2="40" y2="0" stroke="#F6690C" stroke-width="1" />
+                    </svg>
+                </div>
             </div>
-        </div>
-        <div class="content font-outfit text-text-primary text-justify">
-            <ContentDoc />
-        </div>
-        <div class="flex flex-row justify-center my-5 space-x-5">
-            <SocialShare network="facebook" class="border rounded-md p-2 border-accent-primary hover:bg-accent-primary"/>
-            <SocialShare network="twitter" class="border rounded-md p-2 border-accent-primary hover:bg-accent-primary"/>
-            <SocialShare network="linkedin" class="border rounded-md p-2 border-accent-primary hover:bg-accent-primary"/>
-        </div>
-        <div class="h-28">
+            <div class="content font-outfit text-text-primary text-justify">
+                <ContentDoc />
+            </div>
+            <div class="flex flex-row justify-center my-5 space-x-5">
+                <SocialShare network="facebook" class="border rounded-md p-2 border-accent-primary hover:bg-accent-primary"/>
+                <SocialShare network="twitter" class="border rounded-md p-2 border-accent-primary hover:bg-accent-primary"/>
+                <SocialShare network="linkedin" class="border rounded-md p-2 border-accent-primary hover:bg-accent-primary"/>
+            </div>
+            <div class="h-28">
+            </div>
         </div>
     </main>
 </template>
